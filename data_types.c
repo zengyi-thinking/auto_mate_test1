0ce452c9e13aac97da0444941f6cf0a7ed5a0c80
zengyi-thinking <3355262104@qq.com>
2025-07-24 15:15:13 +0800

    数据类型

---
+++ data_types.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int age = 25;
+    float height = 1.75f;
+    char grade = 'A';
+    double pi = 3.1415926535;
+    
+    printf("年龄: %d\n", age);
+    printf("身高: %.2f米\n", height);
+    printf("成绩: %c\n", grade);
+    printf("圆周率: %.4f\n", pi);
+    return 0;
+}
